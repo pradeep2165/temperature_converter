9ad01697b16986ad428931acbc60b0e221013de9
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-18 08:04:37 +0530

    functions are arranged in component property

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React from "react";
 import "./App.css";
-
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
-}
-
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: "" };
-  }
-
-  handleChange(e) {
-    this.setState({ temperature: e.target.value });
-  }
-
-  render() {
-    const temperature = this.state.temperature;
-    return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-        <BoilingVerdict celsius={parseFloat(temperature)} />
-      </fieldset>
-    );
-  }
-}
-
+import Calculator from "./component/Calculator.js";
 function App() {
   return (
-    <div className="App">
+    <div>
       <Calculator />
     </div>
   );
